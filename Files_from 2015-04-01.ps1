50f516b8d815123b7aa10b11530ef3dc8bf5c59c
jimnorac <118621584+jimnorac@users.noreply.github.com>
2022-12-05 08:46:04 -0500

    Update Files_from 2015-04-01.ps1

--- Files_from 2015-04-01.ps1
+++ Files_from 2015-04-01.ps1
@@ -1,11 +1,11 @@
-﻿$sourcePath  = 'J:\FTP\AcHistory\Upload'
+# Search a folder path for files that contain a specific date embedded in the file name and copt files to another folder
+$sourcePath  = 'J:\FTP\AcHistory\Upload'
 $destination = 'C:\_\2015-04-01'
 
 # if the destination folder does not already exist, create it
 if (!(Test-Path -Path $destination -PathType Container)) {
     $null = New-Item -Path $destination -ItemType Directory
 }
-
 
 Get-ChildItem -Path $sourcePath -Filter '*2015-04-01*.csv' -File -Recurse | ForEach-Object {
     $newName = '{0}' -f $_.Name
